d1a3d29b9be6d0127a3a70a3a1d4916c20b7a34d
ozen-hub <info.ozenhub@gmail.com>
2022-10-26 22:21:27 +0530

    18

--- js/place-order.js
+++ js/place-order.js
@@ -10,6 +10,7 @@ const loadData = () => {
             itemOptions+=`<option value="${response.code}">${response.code}</option>`;
         });
         $('#item_code').append(itemOptions);
+        setItemData();
     }
 
   // load customer Ids
@@ -21,5 +22,27 @@ const loadData = () => {
             customerOptions+=`<option value="${response.id}">${response.id}</option>`;
         });
         $('#customer_id').append(customerOptions);
+        setCustomerData();
     }
+}
+$('#customer_id').change(()=>{
+    setCustomerData();
+});
+function setCustomerData(){
+    let tempCustomerId= $('#customer_id').val();
+    let tempCustomer= customers.find(response=>response.id==tempCustomerId);
+    $('#name').val(tempCustomer.name);
+    $('#address').val(tempCustomer.address);
+    $('#salary').val(tempCustomer.salary);
+}
+
+$('#item_code').change(()=>{
+    setItemData();
+});
+function setItemData(){
+    let tempItemCode= $('#item_code').val();
+    let tempItem= items.find(response=>response.code==tempItemCode);
+    $('#description').val(tempItem.description);
+    $('#unit-price').val(tempItem.unitPrice);
+    $('#qty-on-hand').val(tempItem.qtyOnHand);
 }
