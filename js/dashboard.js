3a093b1592ae297f3bd20f540f425a7dd19b3413
ozen-hub <info.ozenhub@gmail.com>
2022-10-26 21:07:23 +0530

    14

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,9 @@
 
 loadUserDetails=()=>{
+    $('#item').hide();
+    $('#orders').hide();
+    $('#place-order').hide();
+
     try {
         let userDetails =
             JSON.parse(localStorage.getItem('user'));
@@ -10,4 +14,18 @@ loadUserDetails=()=>{
         window.location.replace('../index.html')
     }
 
+}
+
+function setUi(name){
+    $('#customer').fadeOut(1000);
+    $('#item').fadeOut(1000);
+    $('#orders').fadeOut(1000);
+    $('#place-order').fadeOut(1000);
+
+    switch (name) {
+        case "customer": $('#customer').show(1000);break;
+        case "item":$('#item').show(1000);break;
+        case "place-order":$('#orders').show(1000);break;
+        case "orders":$('#place-order').show(1000);
+    }
 }
